a87624a5cb0596a69696833d0f2e69541608c039
Promode <pramoddutta_96@yahoo.com>
2024-10-19 11:13:55 +0530

    19th -p2

--- src/test/java/com/thetestingacademy/ex_selenium/ex_19102024/Selenium24.java
+++ src/test/java/com/thetestingacademy/ex_selenium/ex_19102024/Selenium24.java
@@ -58,10 +58,18 @@ public class Selenium24 {
         buttonSubmit.click();
 
         // After 3 seconds error comes
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
 
         WebElement error_message = driver.findElement(By.className("notification-box-description"));
         Assert.assertEquals(error_message.getText(),"Your email, password, IP address or location did not match");
+
+
+        driver.quit();
 
 
 
