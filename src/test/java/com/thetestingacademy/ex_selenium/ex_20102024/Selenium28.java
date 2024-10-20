a221156138f8bab95eec8c6ba7124b9e826dc199
Promode <pramoddutta_96@yahoo.com>
2024-10-20 11:42:09 +0530

    20th

---
+++ src/test/java/com/thetestingacademy/ex_selenium/ex_20102024/Selenium28.java
@@ -0,0 +1,26 @@
+package com.thetestingacademy.ex_selenium.ex_20102024;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Selenium28 {
+
+
+    @Description("Verify <add details>")
+    @Test
+    public void test_sample() {
+
+
+
+
+
+
+    }
+}
