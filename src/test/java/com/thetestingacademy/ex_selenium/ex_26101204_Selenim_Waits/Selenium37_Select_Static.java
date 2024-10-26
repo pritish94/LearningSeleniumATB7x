8c668c4c1012d4f8ebf8adfd56d565f1a183475f
Promode <pramoddutta_96@yahoo.com>
2024-10-26 11:56:51 +0530

    26th P2

---
+++ src/test/java/com/thetestingacademy/ex_selenium/ex_26101204_Selenim_Waits/Selenium37_Select_Static.java
@@ -0,0 +1,29 @@
+package com.thetestingacademy.ex_selenium.ex_26101204_Selenim_Waits;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Selenium37_Select_Static {
+    public static void main(String[] args) {
+
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        System.out.println(driver.getTitle());
+        driver.manage().window().maximize();
+
+        WebElement element_select= driver.findElement(By.id("dropdown"));
+        Select select = new Select(element_select);
+//        select.selectByVisibleText("Option 2");
+        select.selectByIndex(2);
+
+
+
+
+
+
+
+    }
+}
