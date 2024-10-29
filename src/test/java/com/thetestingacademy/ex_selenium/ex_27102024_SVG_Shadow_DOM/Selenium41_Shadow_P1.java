375e5859cff71fb4401426063a8f4fc37cf150ef
Promode <pramoddutta_96@yahoo.com>
2024-10-29 07:03:58 +0530

    27

---
+++ src/test/java/com/thetestingacademy/ex_selenium/ex_27102024_SVG_Shadow_DOM/Selenium41_Shadow_P1.java
@@ -0,0 +1,54 @@
+package com.thetestingacademy.ex_selenium.ex_27102024_SVG_Shadow_DOM;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Selenium41_Shadow_P1 {
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--guest");
+        driver = new EdgeDriver(options);
+    }
+
+
+    @Description("Verify Shadow DOM")
+    @Test
+    public void test_shadow_dom(){
+
+        driver.manage().window().maximize();
+        String URL = "https://selectorshub.com/xpath-practice-page/";
+        driver.get(URL);
+        driver.manage().window().maximize();
+        // local-name() , name()- Xpath (built in)
+        WebElement pizza = driver.findElement(By.xpath("//input[@id='pizza']"));
+        pizza.sendKeys("farmhouse");
+
+
+
+
+
+
+
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
